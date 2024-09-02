8d9f2bb1e73a85898ddc0e9976d887f7fbbe35fb
dn.artego <dn.artego@gmail.com>
2024-09-02 14:18:02 +0300

    update

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 function something() {
-  var x = window.localStorage.getItem('score');
-
-  window.localStorage.setItem('score', 555);
+  var x = window.localStorage.getItem('score2');
+  x = x * 1 + 1;
+  window.localStorage.setItem('score2', x);
 
   alert(x);
 }
