71d9c435e5b1472192895f9e414baee56edccacc
dn.artego <dn.artego@gmail.com>
2024-09-04 16:31:34 +0300

    add sum items localstorage

--- public/script.js
+++ public/script.js
@@ -1,3 +1,9 @@
+/**
+ * Increases a counter stored in local storage and alerts it.
+ * The counter is stored under the key 'score2'.
+ * If the key does not exist, it is created with the value 1.
+ * Otherwise, it is increased by 1.
+ */
 function something() {
   var x = window.localStorage.getItem('score2');
   x = x * 1 + 1;
@@ -6,9 +12,24 @@ function something() {
   alert(x);
 }
 
+// Function that adds a product to the cart by its id
+// It uses a local storage to keep track of the number of products
+// in the cart
+// @param id - id of the product to add to the cart
 function addToCart(id) {
+  // The key for the local storage
   var key = 'product' + id;
+  // Get the current number of products in the cart
   var x = window.localStorage.getItem(key);
+  // Increment the number of products
   x = x * 1 + 1;
+  // Save the new number of products in the cart
   window.localStorage.setItem(key, x);
+}
+
+function printCart() {
+  const hh = window.localStorage
+  const sum = Object.values(hh).reduce((accumulator, currentValue) => accumulator + Number(currentValue), 0);
+
+  console.log(sum); // Вывод: 60
 }
