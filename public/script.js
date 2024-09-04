010ca0f6a75d183f0f49aa932845fe9be9f51df2
dn.artego <dn.artego@gmail.com>
2024-09-04 15:58:53 +0300

    add function addToCart

--- public/script.js
+++ public/script.js
@@ -6,6 +6,9 @@ function something() {
   alert(x);
 }
 
-function addToCart() {
- alert('Товар добавлен в корзину');
+function addToCart(id) {
+  var key = 'product' + id;
+  var x = window.localStorage.getItem(key);
+  x = x * 1 + 1;
+  window.localStorage.setItem(key, x);
 }
